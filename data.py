59c1a5a168f42e9ddd1c514945d14e64f3229423
Matthew Jacobsen <matthewjacobsen1999@gmail.com>
2023-02-04 10:22:46 -0700

    Commenting data.py

--- data.py
+++ data.py
@@ -1,6 +1,10 @@
 import numpy as np
 from PIL import Image
 
+'''
+    Manually creating and saving dot images as pngs
+'''
+
 red_dot = np.zeros(shape=(3,3,3),dtype=np.uint8)
 red_dot[1][1][0]=255
 green_dot = np.zeros(shape=(3,3,3),dtype=np.uint8)
@@ -8,17 +12,10 @@ green_dot[1][1][1]=255
 blue_dot = np.zeros(shape=(3,3,3),dtype=np.uint8)
 blue_dot[1][1][2]=255
 
-red_dot = np.zeros(shape=(3,3,3),dtype=np.uint8)
-red_dot[0][1][1]=255
-green_dot = np.zeros(shape=(3,3,3),dtype=np.uint8)
-green_dot[1][1][1]=255
-blue_dot = np.zeros(shape=(3,3,3),dtype=np.uint8)
-blue_dot[2][1][1]=255
-
 r = Image.fromarray(red_dot)
 g = Image.fromarray(green_dot)
 b = Image.fromarray(blue_dot)
 
-r.save("red_dot_n.png")
-g.save("green_dot_n.png")
-b.save("blue_dot_n.png")
+r.save("red_dot.png")
+g.save("green_dot.png")
+b.save("blue_dot.png")
