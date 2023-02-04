260337dfc5da7a1eeb8a098fdd812a5343268657
Matthew Jacobsen <matthewjacobsen1999@gmail.com>
2023-02-04 10:16:33 -0700

    comments on dataset.py

--- dataset.py
+++ dataset.py
@@ -4,6 +4,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 import torch
 
+'''
+    This is our custom Red, Green, Blue dataset
+    it is created with all zeros in a matrix of shape (3,3,3)
+    then the middle pixel is manually changed to be the max (255)
+    value for the color channel, creating a simple dot in the middle
+    of the 3x3 image
+'''
 class CustomImageDataset(Dataset):
     def __init__(self, len):
         self.len = len
@@ -16,6 +23,9 @@ class CustomImageDataset(Dataset):
         
         d = {0:red_dot, 1:green_dot, 2:blue_dot}
         
+        # for testing purposes if we only want one red, green, and blue
+        # to use we just intialize the length of the dataset as 3
+        # otherwise it creates a completely random rgb dataset
         if len != 3:
             self.labels = []
             self.images = []
